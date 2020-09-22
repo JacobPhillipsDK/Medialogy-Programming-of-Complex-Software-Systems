1231a171b358aa4d832f3caeb5efae8fced71fa7
Jacob Phillips <me@jacobphillips.dk>
2020-09-22 13:40:16 +0200

    Har lavet nye class
    Nye classer, og flytter rundt på ting

--- MainJacob.py
+++ MainJacob.py
@@ -14,36 +14,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# str = string
-# Class for player
-class Player:
-    __name = "Default"
-    __Lane = "Default"
-
-    def __init__(self, new_name="Default", new_lane="Default"):
-        self.__name = new_name
-        self.__Lane = new_lane
-
-    def setName(self, new_name: str) -> None:
-        self.__name = new_name
-
-    def getName(self) -> str:
-        return self.__name
-
-    def setLane(self, new_lane: str) -> None:
-        self.__Lane = new_lane
-
-    def getLane(self) -> str:
-        return self.__Lane
-
-
-class PlayerRole(Player):
-    def __init__(self, new_name="Default", new_lane="Default", new_team="None", new_experience=0):
-        super(Player, self).__init__(new_name, new_lane)
-        self.__team = new_team
-        self.__experience = new_experience
-
-    def setLaneMid(self, midLane: str) -> None:
-        self.__Lane = midLane
