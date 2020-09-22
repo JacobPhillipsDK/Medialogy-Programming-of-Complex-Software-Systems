ae8f9ff8f8b6975f67023e0b2a515d90b225f4ff
Jacob Phillips <me@jacobphillips.dk>
2020-09-22 13:26:24 +0200

    Created class

--- MainJacob.py
+++ MainJacob.py
@@ -1,4 +1,5 @@
 import logging
+import random
 import numpy as np
 
 # https://docs.python.org/3/howto/logging.html - Loggin meaning
@@ -21,6 +22,10 @@ class Player:
     __name = "Default"
     __Lane = "Default"
 
+    def __init__(self, new_name="Default", new_lane="Default"):
+        self.__name = new_name
+        self.__Lane = new_lane
+
     def setName(self, new_name: str) -> None:
         self.__name = new_name
 
@@ -32,3 +37,13 @@ class Player:
 
     def getLane(self) -> str:
         return self.__Lane
+
+
+class PlayerRole(Player):
+    def __init__(self, new_name="Default", new_lane="Default", new_team="None", new_experience=0):
+        super(Player, self).__init__(new_name, new_lane)
+        self.__team = new_team
+        self.__experience = new_experience
+
+    def setLaneMid(self, midLane: str) -> None:
+        self.__Lane = midLane
