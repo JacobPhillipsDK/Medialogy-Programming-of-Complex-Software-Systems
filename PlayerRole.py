777da0a147307b32d073043511d9fda55be3f646
poul <ktromb19@student.aau.dk>
2020-09-24 11:31:04 +0200

    hej
    ghe

--- PlayerRole.py
+++ PlayerRole.py
@@ -11,19 +11,32 @@ class PlayerRole(Player):
         self.__experience = new_experience
         self.__reaction = new_reaction
 
-    def getMotivation(self):
+    def getMotivation(self) -> int:
         return self.__motivation
 
-    def getExperience(self):
+    def getExperience(self) -> int:
         return self.__experience
 
-    def getReaction(self):
+    def getReaction(self) -> int:
         return self.__reaction
 
+<<<<<<< Updated upstream
     def tryKill(self, PlayerRole):
+=======
+
+
+    def trykill(self, defff: int) -> bool:
+>>>>>>> Stashed changes
         r = random.randint(-2, 2)
-        kill = (self.getExperience() * (self.getMotivation() * 0.5)) + r
-        if kill >= PlayerRole.getReaction():
+        kill = self.getExperience() + (self.getMotivation()) + r
+        if kill >= self.trydeff():
             return True
         else:
             return False
+
+
+    def trydeff(self) -> int:
+        r1 = random.randint(-2,2)
+        deff = self.getExperience() + self.getReaction() + r1
+        return deff
+
