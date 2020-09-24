8ec88c6123661272ba8b4d2a0294f056d12ad6bf
Pixlow <mcpixlow@gmail.com>
2020-09-24 10:35:53 +0200

    Ny Class Champions.py og ændringer i ChampionPicker.py

--- ChampionPicker.py
+++ ChampionPicker.py
@@ -1,5 +1,5 @@
-import Player
-from random import *
+from numpy import random
+from Champions import champion
 
 top = int
 jgl = int
@@ -7,35 +7,81 @@ mid = int
 adc = int
 sup = int
 
+#Toplane champs
+Garen = champion()
+Garen.setName("Garen")
+Garen.setLane("top")
+
+Darius = champion()
+Darius.setName("Darius")
+Darius.setLane("top")
+
+#Jungle Champs
+Fiddlesticks = champion()
+Fiddlesticks.setName("Fiddlesticks")
+Fiddlesticks.setLane("jgl")
+
+Warwick = champion()
+Warwick.setName("Warwick")
+Warwick.setLane("jgl")
+
+#Mid Champs
+Ahri = champion()
+Ahri.setName("Ahri")
+Ahri.setLane("mid")
+
+Lux = champion()
+Lux.setName("Lux")
+Lux.setLane("mid")
+
+#ADC Champs
+Vayne = champion()
+Vayne.setName("Vayne")
+Vayne.setLane("adc")
+
+Draven = champion()
+Draven.setName("Draven")
+Draven.setLane("adc")
+
+#Support Champs
+Soraka = champion()
+Soraka.setName("Soraka")
+Soraka.setLane("sup")
+
+Alistar = champion()
+Alistar.setName("Alistar")
+Alistar.setLane("sup")
+
 def get_top_champion(top):
     switcher = {
-    0: "Garen",
-    1: "Darius",
+    0: Garen.getName(),
+    1: Darius.getName(),
 }
 
 def get_jgl_champion(jgl):
     switcher = {
-    0: "Fiddlesticks",
-    1: "Warwick",
+    0: Fiddlesticks,
+    1: Warwick,
 }
 
 def get_mid_champion(mid):
     switcher = {
-    0: "Ahri",
-    1: "Lux",
+    0: Ahri,
+    1: Lux,
 }
 
 def get_adc_champion(adc):
     switcher = {
-    0: "Vayne",
-    1: "Draven",
+    0: Vayne,
+    1: Draven,
 }
 
 def get_sup_champion(sup):
     switcher = {
-    0: "Soraka",
-    1: "Alistar",
+    0: Soraka,
+    1: Alistar,
 }
 
-if  Player.__lane == "top":
-    print (get_top_champion(random(0-1)))
+#Brug if statement til at hente champs, og switch til at store champions.
+
+print (get_mid_champion(0))
