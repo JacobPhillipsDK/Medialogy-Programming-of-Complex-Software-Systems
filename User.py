54222417708b5f24d1543c0d766a89997a327640
poul <ktromb19@student.aau.dk>
2020-10-20 10:08:50 +0200

    xd
    xd

--- User.py
+++ User.py
@@ -1,1 +1,14 @@
 class User:
+    def __innit__(self,name: str, age: str, money: int):
+        self.__name = name
+        self.__age = age
+        self.__money = money
+
+    def get_Name(self):
+        return self.__name
+
+    def get_Money(self):
+        return self.__money
+
+    def set_money(self,newmoney: int):
+        self.__money = newmoney
