9d60fbcbde546a46e4800aad0a74d265c31c5851
poul <ktromb19@student.aau.dk>
2020-10-20 12:06:00 +0200

    GUI SHIT
    gui

--- GUI - Kristian.py
+++ GUI - Kristian.py
@@ -2,8 +2,21 @@ import tkinter as tk
 from tkinter import filedialog,Text
 import os
 
-frontscreen = tk.Tk()
+vindueSkift = 0
 
+def vindueSkift():
+    newWin = tk.Toplevel()
+    newWin.title('second window')
+
+
+def destroyWin():
+    frontscreen.destroy()
+
+
+
+
+
+frontscreen = tk.Tk()
 labelWelcome = tk.Label(frontscreen,text="Welcome to LoL manager!",padx=100,pady=10,font=(None,30),fg = "red")
 labelWelcome.pack()
 labelName = tk.Label(frontscreen,text="Enter your desired name")
@@ -11,7 +24,9 @@ labelName.pack()
 name = tk.Entry(frontscreen)
 name.pack()
 name.get()
-beginGame = tk.Button(frontscreen, text="Begin Game",padx=20,pady=10,fg="black",bg="black")
+beginGame = tk.Button(frontscreen, text="Begin Game",padx=20,pady=10,fg="black",bg="black",command=vindueSkift)
 beginGame.pack()
 
-frontscreen.mainloop()
+
+
+
