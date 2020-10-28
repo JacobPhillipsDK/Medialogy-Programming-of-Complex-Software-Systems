a6058a8db04a3f9a81d99b9a475a32011056f83a
poul <ktromb19@student.aau.dk>
2020-10-28 10:10:47 +0100

    Update main2.py

--- main2.py
+++ main2.py
@@ -1,19 +1,31 @@
 import gameSimulator as GS
 
+<<<<<<< Updated upstream
 
 def Search(arry, value):
     for i in range(len(players1)):
         if arry[i].getName() == value:
             print(arry[i].getName())
+=======
+def Search(list, value):
+    for i in range(len(players1)):
+        if list[i].getCost() == value:
+            print(list[i].getName() + " The value was found at", i + 1)
+>>>>>>> Stashed changes
             break
 
 
 def sortafterCost(sorted):
     for i in range(len(allPlayers) - 1, 0, -1):
         for j in range(i):
+<<<<<<< Updated upstream
             if allPlayers[j].getCost() > allPlayers[j + 1].getCost():
                 sorted[j], sorted[j + 1] = sorted[j + 1], sorted[j]
 
+=======
+            if allPlayers[j].getCost()>allPlayers[j+1].getCost():
+                sorted[j], sorted[j+1] = sorted[j+1], sorted[j]
+>>>>>>> Stashed changes
 
 allPlayers = []
 
@@ -64,6 +76,7 @@ game = GS.gameSimulator()
 game.game_start(blueteam, redteam)
 
 
+<<<<<<< Updated upstream
 for i in range(len(players1) - 1, 0, -1):
     for j in range(i):
         if players1[j].getCost() > players1[j + 1].getCost():
@@ -72,3 +85,10 @@ for i in range(len(players1) - 1, 0, -1):
 sortafterCost(allPlayers)
 for i in range(len(allPlayers)):
     print(allPlayers[i].getName())
+=======
+sortafterCost(allPlayers)
+for i in range(len(allPlayers)):
+    print(allPlayers[i].getName())
+
+Search(allPlayers,20)
+>>>>>>> Stashed changes
