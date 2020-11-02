5381850b555a1c9f102cb9ee3e4aa4a0a183226c
Pixlow <mcpixlow@gmail.com>
2020-11-02 09:47:42 +0100

    Client og server.py update, output scrubber

--- client.py
+++ client.py
@@ -1,15 +1,22 @@
 import socket
 
+#Laver ny socket
 s = socket.socket()
 
+#Port skal matches med server port
 port = 40000
 
-s.connect(('192.168.1.103', port))
+#Sættes til IPv4 som serveren køres på
+s.connect(('192.168.1.3', port))
 
+#Buffer size skal være 4096 eller derover ellers bliver alt data ikke sendt.
 Message = s.recv(4096)
+
 #Str skal decodes når de bliver modtaget
 print(Message.decode('ascii'))
 
+#Åbner output.txt i write mode for at slette content, derefter lukkes den
 open("output.txt", "w").close()
 
+#Lukker socket connecetion
 s.close()
--- server.py
+++ server.py
@@ -7,17 +7,23 @@ port = 40000
 
 s.bind(('', port))
 print ("socket binded to %s" %(port) )
-s.listen(5)
+
+s.listen(2)
 print("socket is listening")
+
+#Sletter data i output før et nyt spil startes
+open("output.txt", "w").close()
+
+#Kører et spil på serveren
 startGame()
 
-with open ("output.txt", "r") as File:
-    data=File.read()
+with open("output.txt", "r") as File:
+    data = File.read()
+
 while True:
     c, addr = s.accept()
     print('connection from', addr)
     #Str skal encodes når de sendes
-
     c.send(data.encode('ascii'))
     c.close()
 
