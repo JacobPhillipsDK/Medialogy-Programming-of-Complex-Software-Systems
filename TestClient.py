732ab3e70a33b9bf788898a0aafe2d5f476e28c3
Pixlow <mcpixlow@gmail.com>
2020-11-13 11:14:47 +0100

    TestClient for testing af multi-thread

---
+++ TestClient.py
@@ -0,0 +1,27 @@
+import socket
+
+#Laver ny socket
+c = socket.socket()
+
+
+#Port skal matches med server port
+port = 40000
+
+#Sættes til IPv4 som serveren køres på
+c.connect(('192.168.1.3', port))
+
+#Buffer size skal være 4096 eller derover ellers bliver alt data ikke sendt.
+Message = c.recv(4096)
+
+#Str skal decodes når de bliver modtaget
+print(Message.decode('ascii'))
+
+#Åbner output.txt i write mode for at slette content, derefter lukkes den
+open("output.txt", "w").close()
+
+#Lukker socket connecetion
+while True:
+    Input = input('Say Something: ')
+    c.send(str.encode(Input))
+    Response = c.recv(1024)
+    print(Response.decode('utf-8'))
